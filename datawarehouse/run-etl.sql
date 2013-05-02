44abffa6babdd929dc01185070e6b53fe55454a7
Yeb Havinga <yhavinga@gmail.com>
2013-05-02 14:22:30 +0200

    Added run-etl.sql script that is used by datawarehouse/Makefile.

---
+++ datawarehouse/run-etl.sql
@@ -0,0 +1,9 @@
+/*
+ * run-etl.sql
+ *
+ * This file is part of the MGRID HDW sample datawarehouse release.
+ *
+ * Copyright (c) 2013, MGRID BV Netherlands
+ */
+
+SELECT stream_etl_observation_evn();
