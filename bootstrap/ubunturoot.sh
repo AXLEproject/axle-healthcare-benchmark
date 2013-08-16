cad39b9f3db84e2336c082e9b4f91a8058875fcd
Yeb Havinga <yhavinga@gmail.com>
2013-08-16 16:13:41 +0200

    Configure ubuntu to use Java 7, which is needed by the CDA generator.

--- bootstrap/ubunturoot.sh
+++ bootstrap/ubunturoot.sh
@@ -19,6 +19,7 @@ apt-get install -y libxml2-dev make libreadline-dev zlib1g-dev libossp-uuid-dev
 # packages for the cda generator
 # install 7 explicitly, since the generator does not work on java 6, ubuntu 12.04's default
 apt-get install -y openjdk-7-jdk
+update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
 apt-get install -y maven2
 
 # packages for the message parser
