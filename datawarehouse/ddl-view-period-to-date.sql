90a69a73784b2b6401378884f6de5585a68185b7
Henk Enting <h.d.enting@mgrid.net>
2013-10-08 17:00:01 +0200

    Rename ytd_patients_per_code to patients_per_provider.

--- datawarehouse/ddl-view-period-to-date.sql
+++ datawarehouse/ddl-view-period-to-date.sql
@@ -6,23 +6,22 @@
  * Copyright (c) 2013, MGRID BV Netherlands
  */
 
-DROP TYPE IF EXISTS ytd_patients_per_code CASCADE;
-CREATE TYPE ytd_patients_per_code AS (
+DROP TYPE IF EXISTS patients_per_provider CASCADE;
+CREATE TYPE patients_per_provider AS (
   provider_sk   INTEGER
 , patient_sk    INTEGER
 );
 
 /*
- * ytd_patients_per_code returns a set of (provider_sk, patient_sk) tuples of facts
+ * patients_per_provider(code, codesystem, reference_date, interval) returns a set of (provider_sk, patient_sk) tuples of facts
  * that:
  * - have a code that is subsumed by the generic concept in_code:in_codesystem
- * - have a from_time that is not older than the interval in_age from in_reference_date
+ * - have a from_time that is not older than the interval from in_reference_date
  */
 
 
-
-CREATE OR REPLACE FUNCTION period_to_date.ytd_patients_per_code(in_code TEXT, in_codesystem TEXT, in_reference_date TIMESTAMPTZ, in_age INTERVAL)
-RETURNS SETOF ytd_patients_per_code AS $$
+CREATE OR REPLACE FUNCTION period_to_date.patients_per_provider(in_code TEXT, in_codesystem TEXT, in_reference_date TIMESTAMPTZ, in_interval INTERVAL)
+RETURNS SETOF patients_per_provider AS $$
     SELECT provider_sk, patient_sk
     FROM (SELECT provider_sk, patient_sk, concept_sk, from_time_sk FROM fact_observation_evn_cv
           UNION ALL
@@ -44,7 +43,7 @@ $$ LANGUAGE SQL;
 
 CREATE OR REPLACE VIEW period_to_date.ytd_patients_with_foot_problems AS
 SELECT provider_sk, COUNT(distinct patient_sk)
-FROM period_to_date.ytd_patients_per_code(
+FROM period_to_date.patients_per_provider(
   '299478007' -- foot problem
 , '2.16.840.1.113883.6.96' -- SNOMED CT
 , current_timestamp
@@ -55,7 +54,7 @@ GROUP BY provider_sk
 
 CREATE OR REPLACE VIEW period_to_date.ytd_patients_with_foot_abnormalities AS
 SELECT provider_sk, COUNT(distinct patient_sk)
-FROM period_to_date.ytd_patients_per_code(
+FROM period_to_date.patients_per_provider(
   '309597007'              -- Foot abnormality - diabetes-related
 , '2.16.840.1.113883.6.96' -- SNOMED CT
 , current_timestamp
@@ -66,7 +65,7 @@ GROUP BY provider_sk
 
 CREATE OR REPLACE VIEW period_to_date.ytd_patients_with_foot_amputations AS
 SELECT provider_sk, COUNT(distinct patient_sk)
-FROM period_to_date.ytd_patients_per_code(
+FROM period_to_date.patients_per_provider(
   '299653001'              -- Amputated foot
 , '2.16.840.1.113883.6.96' -- SNOMED CT
 , current_timestamp
@@ -77,7 +76,7 @@ GROUP BY provider_sk
 
 CREATE OR REPLACE VIEW period_to_date.ytd_patients_with_ulcers AS
 SELECT provider_sk, COUNT(distinct patient_sk)
-FROM period_to_date.ytd_patients_per_code(
+FROM period_to_date.patients_per_provider(
   '201251005'              -- Neuropathic diabetic ulcer - foot
 , '2.16.840.1.113883.6.96' -- SNOMED CT
 , current_timestamp
@@ -89,7 +88,7 @@ GROUP BY provider_sk
 
 CREATE OR REPLACE VIEW period_to_date.ytd_patients_with_examinations AS
 SELECT provider_sk, COUNT(distinct patient_sk)
-FROM period_to_date.ytd_patients_per_code(
+FROM period_to_date.patients_per_provider(
   '401191002' -- foot examination
 , '2.16.840.1.113883.6.96' -- SNOMED CT
 , current_timestamp
