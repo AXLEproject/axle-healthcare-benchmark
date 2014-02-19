bd05de974bdb22609bcf40099366e98e91b521ec
Fabian Walraven <f.a.walraven@mgrid.net>
2014-02-19 11:41:40 +0100

    Add test script for local instance testing
    Allows for a local VM to be used instead of an amazon instance by faking
    euca tools.
    
    Assumes a local CentOS VM to be running with some prerequisites:
    - user with sudo rights
    - mounted storage on /media/ephemeral0 (can be symlink to local disk)
    - key-based authentication by settings ~/.ssh/authorized_keys
    
    The user and key to be used by the test script can be passed in as
    parameters, as well as some other options, see "test-instance.sh -h"

--- messaging/deploy-tools/start-instance.sh
+++ messaging/deploy-tools/start-instance.sh
@@ -12,7 +12,7 @@
 # - copy the password
 #
 
-if [ $# -ne 9 ];
+if [ $# -ne 10 ];
 then
     echo "Usage: $0 <ami> <amiusername> <keypairname> <keypairfile> <region> <instancetype> <groupname> <instancename> <broker-host> <dwh-host>"
     exit 127
@@ -28,6 +28,11 @@ GROUPNAME="$7"
 INSTANCENAME="$8"
 BROKERHOST="$9"
 DWHHOST="$10"
+
+# allow some settings to be passed via the environment (for testing)
+SSHPORT=${SSHPORT:-22}
+INSTANCEWAIT=${INSTANCEWAIT:-25}
+LOGINWAIT=${LOGINWAIT:-10}
 
 echo "Starting instance: ami $1 amiusername $2 keypairname $4 keypairuser $5 ec2_region $5 instancetype $6 groupname $7 instancename $8 brokerhost $9 dwhhost $10"
 
@@ -60,7 +65,7 @@ ZONE=`euca-describe-instances --filter instance-id=${INSTANCE} | tr '\n' ' ' | a
 echo "Succesfully runned ${INSTANCE} on zone ${ZONE}"
 
 # It takes about 30 seconds to start the instance
-sleep 25
+sleep ${INSTANCEWAIT}
 
 while ! test `euca-describe-instances --filter instance-id=${INSTANCE} | tr '\n' ' ' | awk '{print $9}'` = "running"; do
 	echo "Waiting for the instance to become running"
@@ -75,19 +80,19 @@ echo "Succesfully started ${INSTANCE} running on ip ${IP}"
 RES=`euca-create-tags --tag groupname=${GROUPNAME} --tag instancename=${INSTANCENAME} ${INSTANCE}` \
         || _error "Could not create tags '${GROUPNAME}' and '${INSTANCENAME}' for instance ${INSTANCE} on region ${EC2_REGION}"
 
-while ! test `nmap ${IP} -PN -p ssh | grep tcp | awk '{print $2}'` = "open"
+while ! test `nmap ${IP} -PN -p ${SSHPORT} | grep tcp | awk '{print $2}'` = "open"
 do
 	echo "Waiting for SSH to be available on ${IP}"
 	sleep 2
 done
 
 # Need to sleep a bit more since otherwise we cannot login
-sleep 10
+sleep ${LOGINWAIT}
 
 # On the official CentOS image we need to create the ec2-user
 if [ "X$AMI" = "Xami-230b1b57" ];
 then
-    ssh -t -t -i ${KEYPAIR} -o StrictHostKeyChecking=no root@${IP} <<EOF
+    ssh -p ${SSHPORT} -t -t -i ${KEYPAIR} -o StrictHostKeyChecking=no root@${IP} <<EOF
 adduser ec2-user
 cp -a .ssh ~ec2-user
 chown -R ec2-user.ec2-user ~ec2-user/.ssh
@@ -99,7 +104,7 @@ fi
 # On the hs1.8xlarge instance we must manually mount the EBS volume
 if [ "X$INSTANCETYPE" = "Xhs1.8xlarge" ];
 then
-    ssh -t -t -i ${KEYPAIR} -o StrictHostKeyChecking=no ec2-user@${IP} <<EOF
+    ssh -p ${SSHPORT} -t -t -i ${KEYPAIR} -o StrictHostKeyChecking=no ec2-user@${IP} <<EOF
 sudo mkfs.ext4 -m 0 /dev/xvdf
 sudo sed -i 's/sdb/xvdf/' /etc/fstab
 sudo mount -a
@@ -109,7 +114,7 @@ fi
 
 # The double -t is necessary to not cause sudo to given an error about
 # incorrect terminal settings
-ssh -t -t -i ${KEYPAIR} -o StrictHostKeyChecking=no ${AMIUSERNAME}@${IP} <<EOF
+ssh -p ${SSHPORT} -t -t -i ${KEYPAIR} -o StrictHostKeyChecking=no ${AMIUSERNAME}@${IP} <<EOF
 T=`mktemp`
 sudo yum install -y git
 sudo git init /media/ephemeral0/axle-healthcare-benchmark
@@ -118,7 +123,7 @@ sudo ln -s /media/ephemeral0/axle-healthcare-benchmark axle-healthcare-benchmark
 exit
 EOF
 
-ssh -t -t -i ${KEYPAIR} -o StrictHostKeyChecking=no ${AMIUSERNAME}@${IP} <<EOF
+ssh -p ${SSHPORT} -t -t -i ${KEYPAIR} -o StrictHostKeyChecking=no ${AMIUSERNAME}@${IP} <<EOF
 echo "export PS1=\"[\u@${GROUPNAME}-${INSTANCENAME} \W]\$ \"" >> ~/.bashrc
 exit
 EOF
@@ -126,8 +131,10 @@ EOF
 cat >> ~/.ssh/config <<EOF
 Host ${GROUPNAME}-${INSTANCENAME}
   HostName ${IP}
+  Port ${SSHPORT}
   User ${AMIUSERNAME}
   IdentityFile ${KEYPAIR}
+  StrictHostKeyChecking no
 EOF
 
 pushd $(git rev-parse --show-cdup)
@@ -138,7 +145,7 @@ popd
 
 # Install axle-healthcare-benchmark project including password
 STARTTYPE=`expr match "${INSTANCENAME}" '\(^[a-zA-Z]*\)'`
-ssh -t -t -i ${KEYPAIR} -o StrictHostKeyChecking=no ${AMIUSERNAME}@${IP} <<EOF
+ssh -p ${SSHPORT} -t -t -i ${KEYPAIR} -o StrictHostKeyChecking=no ${AMIUSERNAME}@${IP} <<EOF
 cd axle-healthcare-benchmark
 git checkout topic/fawork/messaging
 exit
@@ -153,11 +160,11 @@ then
     echo "Copying axle password for type $STARTTYPE"
     pwd
     TOPDIR=$(git rev-parse --show-cdup)
-    scp -i ${KEYPAIR} ${TOPDIR}/cda-generator/axle-generator-password.txt ${AMIUSERNAME}@${IP}:axle-healthcare-benchmark/cda-generator/password.txt || _error "Could not copy axle generator password"
+    scp -P ${SSHPORT} -i ${KEYPAIR} ${TOPDIR}/cda-generator/axle-generator-password.txt ${AMIUSERNAME}@${IP}:axle-healthcare-benchmark/cda-generator/password.txt || _error "Could not copy axle generator password"
 fi
 
 # Start the setup script based on the instance name
-ssh -t -t -i ${KEYPAIR} -o StrictHostKeyChecking=no ${AMIUSERNAME}@${IP} <<EOF
+ssh -p ${SSHPORT} -t -t -i ${KEYPAIR} -o StrictHostKeyChecking=no ${AMIUSERNAME}@${IP} <<EOF
 cd
 sudo ./axle-healthcare-benchmark/bootstrap/centos-setup-${STARTTYPE}.sh ${BROKERHOST} ${DWHHOST} ${AMIUSERNAME}
 exit
---
+++ messaging/deploy-tools/test-instance.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+#
+# axle-healthcare-benchmark
+#
+# Copyright (c) 2014, Portavita B.V.
+#
+# Perform start-instance on local VM
+
+set -x
+
+usage() {
+cat << EOF
+usage: $0 [OPTIONS]
+
+Perform start-instance on local VM
+
+OPTIONS:
+   -h      Show this message
+   -H      Hostname of VM
+   -P      SSH port of VM
+   -I      Instance name to test
+   -K      Private key to use for SSH session
+   -U      Username to use on VM
+   -B      Broker hostname
+   -D      DWH hostname
+EOF
+}
+
+VMHOST="localhost"
+VMPORT="22"
+INSTANCE="broker"
+KEY="~/.ssh/id_rsa"
+VMUSER="vagrant"
+BROKERHOST="localhost"
+DWHHOST="localhost"
+GROUP="itest"
+
+while getopts ":hH:P:I:K:U:" opt; do
+        case $opt in
+        h)
+                usage
+                exit 1
+        ;;
+        H)
+                VMHOST=$OPTARG
+        ;;
+        P)
+                VMPORT=$OPTARG
+        ;;
+        I)
+                INSTANCE=$OPTARG
+        ;;
+        K)
+                KEY=$OPTARG
+        ;;
+        U)
+                VMUSER=$OPTARG
+        ;;
+        B)
+                BROKERHOST=$OPTARG
+        ;;
+        D)
+                DWHHOST=$OPTARG
+        ;;
+        \?)
+                echo "Invalid option: -$OPTARG" >&2
+        ;;
+        esac
+done
+
+function _log {
+  echo "LOCALTEST: $@" >&2
+}
+
+function euca-run-instances {
+  _log "euca-run-instances $@"
+  cat <<-EOF
+RESERVATION	r-dc7b4f9f	736077515261
+INSTANCE	i-bf4450fc	ami-ce10e0b9		ip-172-31-15-122.eu-west-1.compute.internal	pending	axle0t1.micro	2014-02-19T09:20:26.000Z	eu-west-1b	aki-71665e05			monitoring-disabled	172.31.15.122	vpc-df8861b5	subnet-dc8861b6	ebs
+EOF
+}
+
+function euca-describe-instances {
+  _log "euca-describe-instances $@"
+  cat <<-EOF
+RESERVATION	r-dc7b4f9f	736077515261
+INSTANCE	i-bf4450fc	ami-ce10e0b9	$VMHOST	$VMHOST	running	axle	0		t1.micro	2014-02-19T09:20:26.000Z	eu-west-1b	aki-71665e05			monitoring-disabled	54.194.149.54	172.31.15.122	vpc-df8861b5	subnet-dc8861b6	ebs
+TAG	instance	i-bf4450fc	groupname	$GROUP
+TAG	instance	i-bf4450fc	instancename	$INSTANCE
+EOF
+}
+
+function euca-create-tags {
+  _log "euca-create-tags $@"
+}
+
+function ssh {
+  _log "ssh $@"
+  /usr/bin/ssh -p $VMPORT $@
+}
+
+function nmap {
+  _log "nmap $@"
+  cat <<-EOF
+Starting Nmap 6.40 ( http://nmap.org ) at 2014-02-19 10:45 CET
+Nmap scan report for $VMHOST ($VMHOST)
+Host is up (0.00014s latency).
+PORT   STATE SERVICE
+$VMPORT/tcp open  ssh
+
+Nmap done: 1 IP address (1 host up) scanned in 0.03 seconds
+EOF
+}
+
+export SSHPORT=$VMPORT INSTANCEWAIT=0 LOGINWAIT=0
+
+source ./start-instance.sh ami-ce10e0b9 $VMUSER $VMUSER $KEY eu-west-1 c3.large itest $INSTANCE $BROKERHOST $DWHHOST
