bdbf31882eac9ddbd0f0ef8d67914c85697e0a86
Yeb Havinga <yhavinga@gmail.com>
2014-04-16 02:20:30 -0700

    Add attributes for entity resolution to all RIM classes.
    Attributes are:
    
    * _id_cluster     - refers to the records latest version.
    * _weight         - sum of the weights for each non-null attribute.
    * _type_1_hash    - hash of all type 1 attributes.
    * _type_2_hash    - hash of all type 2 attributes.
    * scoper_original - for each fk attribute X, add X_original to contain the
      reference to the original version, in cases there X is updated to the latest
      version. We need to update X in cases where the original version contains no
      information, such as a Patient record with only a id::II. In that case we
      must change the reference to the clusters latest record version that has more
      weight.
    
    Note that these records are added to ALL RIM classes, not only Entity and Role,
    as this allows to apply the same techniques to Act versions as well.
    
    Moved from HDM's rimmif.xsl are:
    
    * Participation._origin
    * _pond_timestamp - added to the ponds with default timestamp, and the lake.
    * _lake_timestmap - added to the lake with default timestamp.

--- lake/create_lake.sh
+++ lake/create_lake.sh
@@ -44,12 +44,19 @@ pgext2sql_unlogged() {
     echo "Manually loading extension $2"
     EXTDIR=$(pg_config --sharedir)/extension
     cat ${EXTDIR}/$2 | sed -e 's/MODULE_PATHNAME/\$libdir\/hl7/g' \
-        -e 's/CREATE TABLE/CREATE UNLOGGED TABLE/g' \
-        -e 's/_id BIGSERIAL PRIMARY KEY,/_id BIGSERIAL PRIMARY KEY, _cluster BIGINT,/g' \
+        -e 's/PRIMARY KEY,/PRIMARY KEY, _id_cluster BIGINT,/g' \
+        -e 's/_clonename TEXT,/_clonename TEXT, _pond_timestamp TIMESTAMPTZ, _lake_timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP, _type_1_hash INT, _type_2_hash INT, _weight INT,/g' \
+        -e 's/"source" BIGINT, "target" BIGINT,/"source" BIGINT, "target" BIGINT, "source_original" BIGINT, "target_original" BIGINT,/g' \
+        -e 's/"act" BIGINT, "role" BIGINT,/"act" BIGINT, "role" BIGINT, "act_original" BIGINT, "role_original" BIGINT,/g' \
+        -e 's/"player" BIGINT, "scoper" BIGINT,/"player" BIGINT, "scoper" BIGINT, "player_original" BIGINT, "scoper_original" BIGINT,/g' \
+        -e '/CREATE TABLE "[[:alpha:]]*Participation"/ {s/_clonename TEXT,/_clonename TEXT, _origin BIGINT,/}' \
         -e 's/"value" "ANY",/_value_pq_value NUMERIC, _value_pq_unit TEXT, _value_code_code TEXT, _value_code_codesystem TEXT, "value" "ANY",/g' \
         -e 's/, "effectiveTime"/, _effective_time_low TIMESTAMPTZ, _effective_time_low_year INT, _effective_time_low_month INT, _effective_time_low_day INT, _effective_time_high TIMESTAMPTZ, _effective_time_high_year INT, _effective_time_high_month INT, _effective_time_high_day INT, "effectiveTime"/g' \
+        -e 's/CREATE TABLE/CREATE UNLOGGED TABLE/g' \
         | PGOPTIONS='--client-min-messages=warning' ${PSQL} -q1 --dbname $1 --log-file=log.txt || fail "could not load SQL script from extension $2, see log.txt"
 }
+
+# WITH (appendonly = true, orientation = column, compresslevel = 9)\
 
 gpext2sql() {
     echo "Manually loading extension $2"
@@ -65,25 +72,38 @@ gpext2sql() {
         -e 's/\([^a-z]cv"*\)([^)]\+)/\1/gi' \
         -e 's/\([^a-z]cs"*\)([^)]\+)/\1/gi' \
         -e 's/\([^a-z]set"*\)([^)]\+)/\1/gi' \
-        -e 's/_id BIGSERIAL PRIMARY KEY,/_id BIGSERIAL PRIMARY KEY, _cluster BIGINT,/g' \
-        -e 's/_id BIGINT PRIMARY KEY,/_id BIGINT PRIMARY KEY, _cluster BIGINT,/g' \
+        -e 's/PRIMARY KEY,/PRIMARY KEY, _id_cluster BIGINT,/g' \
+        -e 's/_clonename TEXT,/_clonename TEXT, _pond_timestamp TIMESTAMPTZ, _lake_timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP, _type_1_hash INT, _type_2_hash INT, _weight INT,/g' \
+        -e 's/"source" BIGINT, "target" BIGINT,/"source" BIGINT, "target" BIGINT, "source_original" BIGINT, "target_original" BIGINT,/g' \
+        -e 's/"act" BIGINT, "role" BIGINT,/"act" BIGINT, "role" BIGINT, "act_original" BIGINT, "role_original" BIGINT,/g' \
+        -e 's/"player" BIGINT, "scoper" BIGINT,/"player" BIGINT, "scoper" BIGINT, "player_original" BIGINT, "scoper_original" BIGINT,/g' \
+        -e '/CREATE TABLE "[[:alpha:]]*Participation"/ {s/_clonename TEXT,/_clonename TEXT, _origin BIGINT,/}' \
         -e 's/ INHERITS (\"[[:alpha:]]*\")//g' \
         -e 's/ PRIMARY KEY//g' \
         -e 's/ REFERENCES \"[[:alpha:]]*\"//g' \
         -e 's/"value" "ANY",/_value_pq_value NUMERIC, _value_pq_unit TEXT, _value_code_code TEXT, _value_code_codesystem TEXT, "value" "ANY",/g' \
         -e 's/, "effectiveTime"/, _effective_time_low TIMESTAMPTZ, _effective_time_low_year INT, _effective_time_low_month INT, _effective_time_low_day INT, _effective_time_high TIMESTAMPTZ, _effective_time_high_year INT, _effective_time_high_month INT, _effective_time_high_day INT, "effectiveTime"/g' \
+        -e '/CREATE TABLE "Act"/ {s/;//}' \
+        -e '/CREATE TABLE "Act"/ {a\
+WITH (appendonly = false)\
+DISTRIBUTED BY (_id);
+}' \
+        -e '/CREATE TABLE "Participation"/ {s/;//}' \
+        -e '/CREATE TABLE "Participation"/ {a\
+WITH (appendonly = true, orientation = column, compresslevel = 6)\
+DISTRIBUTED BY (act);
+}' \
         -e '/CREATE TABLE "Observation"/ {s/;//}' \
         -e '/CREATE TABLE "Observation"/ {a\
-with (appendonly = true, orientation = column, compresslevel = 9)\
-distributed by (_id)\
-partition by range (_effective_time_low_year)\
-  subpartition by range (_effective_time_low_month)\
-    subpartition template (\
-      start (1) end (13) every (1),\
-      default subpartition other_months )\
-\
-  (start (2008) end (2016) every (1),\
-   default partition other_years );
+WITH (appendonly = true, orientation = column, compresslevel = 6)\
+DISTRIBUTED BY (_id)\
+PARTITION BY RANGE (_effective_time_low_year)\
+  SUBPARTITION BY RANGE (_effective_time_low_month)\
+    SUBPARTITION TEMPLATE (\
+      START (1) END (13) EVERY (1),\
+      DEFAULT SUBPARTITION other_months )\
+  (START (2008) END (2016) EVERY (1),\
+   DEFAULT PARTITION other_years );
 }' | PGOPTIONS='--client-min-messages=warning' ${PSQL} -q1 --dbname $1 --log-file=log.txt || fail "could not load SQL script from extension $2, see log.txt"
 }
 
