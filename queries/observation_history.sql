b1cd2e3f5ebc24dab3eb805a4e69c7f3228e40d9
Tom van der Weide <t.van.der.weide@portavita.eu>
2014-06-05 16:33:28 +0200

    Add view for observation history

---
+++ queries/observation_history.sql
@@ -0,0 +1,28 @@
+/*
+ * Observation history per patient
+ */
+CREATE OR REPLACE VIEW observation_history AS
+SELECT   ptnt.player                    AS peso_id
+-- TODO: care provision for which this observation was made would be nice to have
+,        obs._id                        AS act_id
+,        obs."code"->>'code'            AS code
+,        obs."code"->>'codeSystem'      AS codesystem
+,        obs._value_code_code           AS coded_value
+,        obs._value_pq_value            AS pq_value
+,        obs._effective_time_low        AS effective_time_low
+,        RANK() OVER (
+           PARTITION BY ptnt.player, obs."code"->>'code', obs."code"->>'codeSystem'
+           ORDER BY obs._effective_time_low DESC, obs._id DESC)
+                                        AS rocky
+,        RANK() OVER (
+           PARTITION BY ptnt.player, obs."code"->>'code', obs."code"->>'codeSystem'
+           ORDER BY obs._effective_time_low ASC, obs._id ASC)
+                                        AS drago
+FROM    "Observation"                      obs
+
+JOIN    "Participation"                    sbj_ptcp
+ON       sbj_ptcp.act                    = obs._id
+AND      sbj_ptcp."typeCode"->>'code'    = 'RCT'
+JOIN    "Patient"                          ptnt
+ON       ptnt._id                        = sbj_ptcp.role
+;
