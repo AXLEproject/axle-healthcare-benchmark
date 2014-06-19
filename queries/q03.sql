6770a70d79f4d88cac57c0d0e61ee97fe1b2defd
Yeb Havinga <yhavinga@gmail.com>
2014-06-19 02:37:08 -0700

    Rename queries to match document numbers.

--- queries/q03.sql
+++
@@ -1,7 +0,0 @@
--- DATABASE staging
-
-SET search_path=public,etl,staging_rim2011,view_snomed_tree,view_templates,hl7_composites,pg_hl7,hl7,"$user";
-
--- Update the patient dimension
-SELECT create_temp_tables();
-SELECT update_dim_patient();
