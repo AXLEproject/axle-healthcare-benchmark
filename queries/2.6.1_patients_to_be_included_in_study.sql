6df5b4c6c01c71bf190b713c373d33d37c2f167e
Yeb Havinga <yhavinga@gmail.com>
2014-06-19 08:26:26 -0700

    Update query 2.6.1 to use Act rather than Observation.

--- queries/2.6.1_patients_to_be_included_in_study.sql
+++ queries/2.6.1_patients_to_be_included_in_study.sql
@@ -1,13 +1,14 @@
 /* Select all patients for which at least one of the following examinatios
  * exists in a specified period of time.
 */
- SELECT DISTINCT obse_ptcp.role          AS ptnt_id
-  FROM  "Observation"                    obse
-  JOIN  "Participation"                  obse_ptcp
-  ON     obse_ptcp.act                   = obse._id
-  AND    obse_ptcp."typeCode"->>'code'   = 'RCT'
-  WHERE  obse._effective_time_low_year BETWEEN 2013 AND 2014
-  AND    obse._effective_time_low >= '20130501'
+SELECT  DISTINCT exam_ptcp.role          AS ptnt_id
+  FROM   ONLY "Act"                       exam
+  JOIN   "Participation"                  exam_ptcp
+  ON     exam_ptcp.act                   = exam._id
+  AND    exam_ptcp."typeCode"->>'code'   = 'RCT'
+  WHERE  '[{"root": "2.16.840.1.113883.2.4.3.31.4.2.1", "dataType": "II", "extension": "1"}]' @> exam."templateId"
+  AND    exam._effective_time_low_year BETWEEN 2013 AND 2014
+  AND    exam._effective_time_low >= '20130501'
   AND   ((_code_code = '170777000' AND _code_codesystem = '2.16.840.1.113883.6.96') -- annual checkup, snomed
          OR
          (_code_code = '170744004' AND _code_codesystem = '2.16.840.1.113883.6.96') -- quarterly checkup, snomed
