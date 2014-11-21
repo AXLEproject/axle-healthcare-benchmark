10a6a700ffd058c35cec1a1a7bb461b020a68ebf
Albana Gaba <a.gaba@portavita.eu>
2014-11-21 14:29:40 +0100

    Add participation ids

--- cda-generator/src/main/scala/eu/portavita/axle/helper/ParticipationHelper.scala
+++ cda-generator/src/main/scala/eu/portavita/axle/helper/ParticipationHelper.scala
@@ -2,11 +2,13 @@ package eu.portavita.axle.helper
 
 import eu.portavita.databus.data.dto.ParticipationDTO
 import java.util.Date
+import eu.portavita.axle.generatable.ParticipationId
 
 object ParticipationHelper {
   
     def createParticipant(id: Long, date: Date, typeCode: String, roleId: Long): ParticipationDTO = {
       val participant = new ParticipationDTO
+      participant.setId(ParticipationId.next)
       participant.setActId(id)
       participant.setFromTime(date)
       participant.setTypeCode(typeCode)
