ff78220525faed5dead3ff5747ec7b7b882b91b6
Yeb Havinga <yhavinga@gmail.com>
2014-11-28 02:36:21 -0800

    Move creation of research_user for RLS to separate script.

---
+++ queries/create_research_schema.sql
@@ -0,0 +1,32 @@
+/*
+ * query      : create_research_schema.sql
+ * description: create schema and user to access research tables
+ * user       : researchers, de-identification required
+ *
+ * Copyright (c) 2014, Portavita B.V.
+ */
+
+DROP SCHEMA IF EXISTS research CASCADE;
+DROP OWNED BY research_user;
+DROP USER IF EXISTS research_user;
+CREATE SCHEMA research;
+
+CREATE USER research_user LOGIN;
+GRANT CONNECT ON DATABASE lake TO research_user;
+ALTER USER research_user SET SEARCH_PATH TO research, pg_hl7, hl7, "$user";
+GRANT USAGE ON SCHEMA research, rim2011, pg_hl7, hl7 TO research_user;
+
+GRANT ALL PRIVILEGES ON SCHEMA research TO research_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA rim2011 TO research_user;
+
+
+/***
+CREATE USER unrestricted_user LOGIN;
+GRANT CONNECT ON DATABASE lake TO unrestricted_user;
+ALTER USER unrestricted_user SET SEARCH_PATH TO public, pg_hl7, hl7, "$user";
+GRANT USAGE ON SCHEMA public, rim2011, pg_hl7, hl7 TO unrestricted_user;
+
+GRANT ALL PRIVILEGES ON SCHEMA public TO unrestricted_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO unrestricted_user;
+***/
+
--- row-security/apply_rls_policies.sql
+++ row-security/apply_rls_policies.sql
@@ -1,26 +1,25 @@
--- Copyright (c) 2014, Portavita BV Netherlands
+/*
+ * apply_rls_policies.sql
+ *
+ * Row Level Security policy for preventing unauthorized users to access
+ * patient records whereby there is an opt-out consent from the patient. The
+ * purpose of use is research.
+ *
+ * Copyright (c) 2014, Portavita BV Netherlands
+ */
 
-/* Row Level Security policy for preventing unauthorized users to access patient records whereby there is an opt-out consent from the patient. The purpose of use is research. */
+SET row_security TO ON;
 
--- User for testing the RLS functionality
-DROP USER IF EXISTS research_user;
-CREATE USER research_user;
+-- Enable row level security on the following tables
 
-GRANT ALL ON ALL TABLES IN SCHEMA rim2011, pg_hl7 TO public;
-GRANT USAGE ON SCHEMA rim2011, pg_hl7 TO public;
-
-SET search_path = rim2011, hl7;
-
-set row_security TO ON;
-
--- enable row level security on the following tables
 ALTER TABLE "Observation" ENABLE ROW LEVEL SECURITY;
 ALTER TABLE "Act" ENABLE ROW LEVEL SECURITY;
 
 DROP POLICY IF EXISTS p10 ON "Observation";
 DROP POLICY IF EXISTS p11 ON "Act";
 
--- Prevents access to those records that are part of a care provision for which the patient has released an opt-out consent.
+-- Prevents access to those records that are part of a care provision for which
+-- the patient has released an opt-out consent.
 
 CREATE POLICY p10 ON "Observation"
 	USING (
