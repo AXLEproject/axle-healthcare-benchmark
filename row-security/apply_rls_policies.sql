4c08817829ced408b23ee0eb3a8cce7df055d612
Yeb Havinga <yhavinga@gmail.com>
2014-11-28 01:00:56 -0800

    Change RLS policies from not-in to not exists to allow subquery pullup.

--- row-security/apply_rls_policies.sql
+++ row-security/apply_rls_policies.sql
@@ -21,26 +21,28 @@ DROP POLICY IF EXISTS p10 ON "Observation";
 DROP POLICY IF EXISTS p11 ON "Act";
 
 -- Prevents access to those records that are part of a care provision for which the patient has released an opt-out consent.
+
 CREATE POLICY p10 ON "Observation"
-	USING (		
-        "Observation"._id not IN (
-            SELECT link."actId" 
-            FROM   "LinkActPcpr" link            
+	USING (
+         NOT EXISTS  (
+            SELECT 1
+            FROM   "LinkActPcpr" link
             JOIN  "OptOutConsent" optout
-            ON    optout."patientId"=link."patientId"
-            AND   optout."careProvision"->>'code'=link."careProvision"->>'code'
-            AND   optout."careProvision"->>'codeSystem'=link."careProvision"->>'codeSystem'
+            ON    optout."patientId"                    = link."patientId"
+            AND   optout."careProvision"->>'code'       = link."careProvision"->>'code'
+            AND   optout."careProvision"->>'codeSystem' = link."careProvision"->>'codeSystem'
             WHERE link."actId" = "Observation"._id
         ));
 
+
 CREATE POLICY p11 ON "Act"
-    USING (     
-        "Act"._id not IN (
-            SELECT link."actId" 
-            FROM   "LinkActPcpr" link            
+    USING (
+        NOT EXISTS (
+            SELECT 1
+            FROM   "LinkActPcpr" link
             JOIN  "OptOutConsent" optout
-            ON    optout."patientId"=link."patientId"
-            AND   optout."careProvision"->>'code'=link."careProvision"->>'code'
-            AND   optout."careProvision"->>'codeSystem'=link."careProvision"->>'codeSystem'
+            ON    optout."patientId"                    =link."patientId"
+            AND   optout."careProvision"->>'code'       =link."careProvision"->>'code'
+            AND   optout."careProvision"->>'codeSystem' =link."careProvision"->>'codeSystem'
             WHERE link."actId" = "Act"._id
         ));
