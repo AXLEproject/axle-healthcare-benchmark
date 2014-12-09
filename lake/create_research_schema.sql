d02f227f6a97534aeca98112161677fd233b2ea9
Yeb Havinga <yhavinga@gmail.com>
2014-12-09 08:49:36 -0800

    Comment drop commands from research schema and user script.
    Since DROP OWNED BY has no IF EXISTS construct, we cannot make this script
    idempotent yet.

--- lake/create_research_schema.sql
+++ lake/create_research_schema.sql
@@ -6,11 +6,14 @@
  * Copyright (c) 2014, Portavita B.V.
  */
 
-DROP SCHEMA IF EXISTS research CASCADE;
-DROP OWNED BY research_user;
-DROP USER IF EXISTS research_user;
-CREATE SCHEMA research;
+/**
+ DROP SCHEMA IF EXISTS research CASCADE;
+ DROP OWNED BY research_user;
+ DROP USER IF EXISTS research_user;
+**/
 
+CREATE SCHEMA research;
+DROP USER IF EXISTS research_user;
 CREATE USER research_user LOGIN;
 GRANT CONNECT ON DATABASE lake TO research_user;
 ALTER USER research_user SET SEARCH_PATH TO research, public, "$user";
@@ -18,7 +21,6 @@ GRANT USAGE ON SCHEMA research, rim2011, pg_hl7, hl7 TO research_user;
 
 GRANT ALL PRIVILEGES ON SCHEMA research TO research_user;
 GRANT SELECT ON ALL TABLES IN SCHEMA rim2011 TO research_user;
-
 
 /***
 CREATE USER unrestricted_user LOGIN;
