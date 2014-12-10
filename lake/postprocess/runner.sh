da84c5d00e80e711ee5a3ddfed9370fe8ef6f27f
Fabian Walraven <f.a.walraven@mgrid.net>
2014-12-10 14:20:53 +0100

    Make lake runner callable in multiple contexts

--- lake/postprocess/runner.sh
+++ lake/postprocess/runner.sh
@@ -7,15 +7,51 @@
 # Exit immediately on error
 set -e
 
-AXLE=/home/${USER}/axle-healthcare-benchmark
+SCRIPTDIR="$(dirname "$0")"
+AXLE=${SCRIPTDIR}/../..
+
+usage() {
+cat << EOF
+usage: $0 [OPTIONS]
+
+OPTIONS:
+   -h      Show this message
+   -p      Lake port
+   -u      Lake user
+   -d      Lake database
+EOF
+}
 
 # ingest part of the default_settings include makefile
 sed -e 's/(/{/g' -e 's/)/}/g' ${AXLE}/default_settings | sed '/shell/d' | sed -n '/^define/,$!p'  > /tmp/default_settings_bash_runner
 source /tmp/default_settings_bash_runner
 
+# allow command line settings to override default settings
+while getopts ":hp:u:d:" opt; do
+        case $opt in
+        h)
+                usage
+                exit 1
+        ;;
+        p)
+                LAKEPORT="${OPTARG}"
+        ;;
+        u)
+                LAKEUSER="${OPTARG}"
+        ;;
+        d)
+                LAKEDB=${OPTARG}
+        ;;
+        \?)
+                echo "Invalid option: -$OPTARG" >&2
+                exit 1
+        ;;
+        esac
+done
+
 PSQL="psql -v ON_ERROR_STOP=true -v VERBOSITY=terse -p ${LAKEPORT} -U ${LAKEUSER} ${LAKEDB}"
 
-cd ${AXLE}/lake/postprocess
+cd ${SCRIPTDIR}
 
 while true
 do PGOPTIONS='--client-min-messages=warning' \
