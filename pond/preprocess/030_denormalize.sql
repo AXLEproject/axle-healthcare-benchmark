8159d1a4bae027ee32e246c15ecb6febb5d5ac28
Yeb Havinga <yhavinga@gmail.com>
2015-08-13 07:35:04 -0700

    Update pond-preprocess to not use deprecated datatype("ANY") function.

--- pond/preprocess/030_denormalize.sql
+++ pond/preprocess/030_denormalize.sql
@@ -28,24 +28,24 @@ UPDATE "Observation"
 SET _value_pq              = value::"PQ"::pq
 ,   _value_pq_value        = value(value::"PQ"::pq)
 ,   _value_pq_unit         = unit(value::"PQ"::pq)
-WHERE datatype(value)      = 'PQ';
+WHERE value->>'dataType'   = 'PQ';
 
 UPDATE "Observation"
 SET _value_code_code       = value ->> 'code'
 ,   _value_code_codesystem = value ->> 'codeSystem'
-WHERE datatype(value)      IN ('CD', 'CS', 'CE', 'CO', 'CV');
+WHERE value->>'dataType'   IN ('CD', 'CS', 'CE', 'CO', 'CV');
 
 UPDATE "Observation"
 SET _value_int             = (value ->> 'value')::int
-WHERE datatype(value)      = 'INT';
+WHERE value->>'dataType'   = 'INT';
 
 UPDATE "Observation"
 SET _value_real            = (value ->> 'value')::numeric
-WHERE datatype(value)      = 'REAL';
+WHERE value->>'dataType'   = 'REAL';
 
 UPDATE "Observation"
 SET _value_ivl_real        = value::"IVL_REAL"::ivl_real
-WHERE datatype(value)      = 'IVL_REAL';
+WHERE value->>'dataType'   = 'IVL_REAL';
 
 /* Effective times */
 
@@ -59,7 +59,7 @@ SET _effective_time_low = lowvalue("effectiveTime"::"IVL_TS"::ivl_ts)::timestamp
 ,   _effective_time_high_year = EXTRACT(year FROM highvalue("effectiveTime"::"IVL_TS"::ivl_ts)::timestamptz)
 ,   _effective_time_high_month = EXTRACT(month FROM highvalue("effectiveTime"::"IVL_TS"::ivl_ts)::timestamptz)
 ,   _effective_time_high_day = EXTRACT(day FROM highvalue("effectiveTime"::"IVL_TS"::ivl_ts)::timestamptz)
-WHERE datatype("effectiveTime") = 'IVL_TS';
+WHERE "effectiveTime"->>'dataType' = 'IVL_TS';
 UPDATE "Act"
 SET _effective_time_low = lowvalue("effectiveTime"::"TS"::ts::ivl_ts)::timestamptz
 ,   _effective_time_low_year = EXTRACT(year FROM lowvalue("effectiveTime"::"TS"::ts::ivl_ts)::timestamptz)
@@ -69,7 +69,7 @@ SET _effective_time_low = lowvalue("effectiveTime"::"TS"::ts::ivl_ts)::timestamp
 ,   _effective_time_high_year = EXTRACT(year FROM highvalue("effectiveTime"::"TS"::ts::ivl_ts)::timestamptz)
 ,   _effective_time_high_month = EXTRACT(month FROM highvalue("effectiveTime"::"TS"::ts::ivl_ts)::timestamptz)
 ,   _effective_time_high_day = EXTRACT(day FROM highvalue("effectiveTime"::"TS"::ts::ivl_ts)::timestamptz)
-WHERE datatype("effectiveTime") = 'TS';
+WHERE "effectiveTime"->>'dataType' = 'TS';
 
 /* Replace this with a single update as soon as we have "GTS"::qset_ts support. */
 UPDATE "Role"
@@ -81,7 +81,7 @@ SET _effective_time_low = lowvalue("effectiveTime"::"IVL_TS"::ivl_ts)::timestamp
 ,   _effective_time_high_year = EXTRACT(year FROM highvalue("effectiveTime"::"IVL_TS"::ivl_ts)::timestamptz)
 ,   _effective_time_high_month = EXTRACT(month FROM highvalue("effectiveTime"::"IVL_TS"::ivl_ts)::timestamptz)
 ,   _effective_time_high_day = EXTRACT(day FROM highvalue("effectiveTime"::"IVL_TS"::ivl_ts)::timestamptz)
-WHERE datatype("effectiveTime") = 'IVL_TS';
+WHERE "effectiveTime"->>'dataType' = 'IVL_TS';
 UPDATE "Role"
 SET _effective_time_low = lowvalue("effectiveTime"::"TS"::ts::ivl_ts)::timestamptz
 ,   _effective_time_low_year = EXTRACT(year FROM lowvalue("effectiveTime"::"TS"::ts::ivl_ts)::timestamptz)
@@ -91,7 +91,7 @@ SET _effective_time_low = lowvalue("effectiveTime"::"TS"::ts::ivl_ts)::timestamp
 ,   _effective_time_high_year = EXTRACT(year FROM highvalue("effectiveTime"::"TS"::ts::ivl_ts)::timestamptz)
 ,   _effective_time_high_month = EXTRACT(month FROM highvalue("effectiveTime"::"TS"::ts::ivl_ts)::timestamptz)
 ,   _effective_time_high_day = EXTRACT(day FROM highvalue("effectiveTime"::"TS"::ts::ivl_ts)::timestamptz)
-WHERE datatype("effectiveTime") = 'TS';
+WHERE "effectiveTime"->>'dataType' = 'TS';
 
 UPDATE "RoleLink"
 SET _effective_time_low = lowvalue("effectiveTime")::timestamptz
