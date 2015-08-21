984c1ba90e88e27c2de06c701226f969c131f8f6
Yeb Havinga <yhavinga@gmail.com>
2015-08-21 16:36:13 +0200

    Change examination routing key so rabbit can route straight to blobstore queue.
    This is to bypass a consume, xml parse and publish cycle by the ingress application
    for the bulk of the CDA generator, which we know are CDA R2 xml documents.

--- cda-generator/src/main/scala/eu/portavita/axle/publisher/RabbitMessageQueue.scala
+++ cda-generator/src/main/scala/eu/portavita/axle/publisher/RabbitMessageQueue.scala
@@ -72,6 +72,6 @@ object RabbitMessageQueue {
 	val practitionerRoutingKey = "generator.fhir.practitioner"
 	val patientRoutingKey = "generator.fhir.patient"
 	val treatmentRoutingKey = "generator.hl7v3.treatment"
-	val examinationRoutingKey = "generator.hl7v3.examination"
+	val examinationRoutingKey = "generator.examination"
   val consentRoutingKey = "generator.hl7v3.consent"
 }
