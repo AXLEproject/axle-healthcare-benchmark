31d423120b4285faf3b108b72830b655cc8b61ec
Yeb Havinga <yhavinga@gmail.com>
2015-09-10 05:11:25 -0700

    Include Role resolution in standard lake postprocess runner.

--- lake/postprocess/010_entity_resolution.sql
+++ lake/postprocess/010_entity_resolution.sql
@@ -9,6 +9,6 @@
 SELECT resolution('rim2011', 'Organization', '{{Role,player},{Role,scoper}}');
 SELECT resolution('rim2011', 'Person'      , '{{Role,player},{Role,scoper}}');
 
---SELECT resolution('rim2011', 'Role'          , '{{Participation_in,role}}');
+SELECT resolution('rim2011', 'Role'          , '{{Participation_in,role}}');
 SELECT resolution('rim2011', 'LicensedEntity', '{{Participation_in,role}}');
 SELECT resolution('rim2011', 'Patient'       , '{{Participation_in,role}}');
