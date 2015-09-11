bb2195565505b32c9b71facfb364598b9d520e23
Yeb Havinga <yhavinga@gmail.com>
2015-09-11 03:10:27 -0700

    Switch to jsonb document store for retinopathy dataset base values source.
    This change also introduces the use of TABLESAMPLE on the Patient table, so the
    returned dataset can be used to 'get a taste of the data', rather than the
    complete dataset.  The sampling percentage depends on the number of patients in
    the database, and it is calculated so the returned sample is in the order of 30
    patients.  The current script uses a fixed sampling seed, so repetitive calls
    return the same data, which is useful for benchmarking and validating results.

--- queries/retinopathy_checks.sql
+++ queries/retinopathy_checks.sql
@@ -11,6 +11,4 @@ SET session_authorization TO research_user;
 SET SEARCH_PATH TO research, public, rim2011, hdl, hl7, r1, "$user";
 
 \echo 'Check that post load document updates have been run.'
-SELECT 1/EXISTS(SELECT * FROM pg_class WHERE relname='document_1_patient_id_id')::int;
-
-
+SELECT 1/EXISTS(SELECT * FROM pg_class WHERE relname='document_1_patient_id_idx')::int;
