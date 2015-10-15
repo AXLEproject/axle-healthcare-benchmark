c26d340e718b78333f5f2d65ed8c66c56587b955
Yeb Havinga <yhavinga@gmail.com>
2015-10-15 00:22:00 -0700

    Reinstate id recording pond preprocess step, for entity resolution

---
+++ pond/preprocess/060_record_ids.sql
@@ -0,0 +1,9 @@
+/*
+ * (c) 2015 MGRID B.V.
+ * All rights reserved
+ *
+ * Pre-process RIM data in a pond before uploading to the lake.  This can be
+ * all pre-processing that does not require knowledge from other documents.
+ */
+
+SELECT pond_recordids();
